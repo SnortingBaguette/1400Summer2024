e45b0edc52dd0411d89bee4d9fa74551476c8d62
Dmitry Panchuk <10994198@uvu.edu>
2024-07-06 00:23:33 -0600

    Finished Unit 9 Lab

--- Unit9Lab/Assets/Scripts/MoveLeft.cs
+++ Unit9Lab/Assets/Scripts/MoveLeft.cs
@@ -6,16 +6,26 @@ public class MoveLeft : MonoBehaviour
 {
 
     private float speed = 30f;
+    private PlayerController playerControllerScript;
+    private float leftBoundary = -15;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        
+        if (transform.position.x < leftBoundary && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Unit9Lab/Assets/Scripts/PlayerController.cs
+++ Unit9Lab/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,29 +7,56 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody playerRb;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
     public float jumpForce = 10f;
     public float gravityModifier = 2f;
     public bool isGrounded = true;
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 0.8f);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isGrounded = true;
+        
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            dirtParticle.Play();
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 0.5f);
+        }
     }
 }
